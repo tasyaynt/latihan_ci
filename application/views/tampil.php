08bfca59e007d90b52543ac8ddcb128694926e60
tasyaynt <tasyaayunita84@gmail.com>
2022-09-20 15:50:47 +0700

    first commit

---
+++ application/views/tampil.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Latihan Pertama</title>
+    </head>
+    <body>
+        <p>Latihan Pertama saya belajar framework Codeigneter</p>
+        <p>Framework Codeigneter sangat mudah dipelajari</p>
+    </body>
+</html>
